885188b002c617acc7c312853493b5b42f90fefc
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-09-07 14:17:19 +0000

    Updated Carrots

--- open/carrots.cob
+++ open/carrots.cob
@@ -3,18 +3,19 @@ PROGRAM-ID. CARROTS.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-   01 N   PIC S9(30).
-   01 M   PIC S9(30).
-   01 K   PIC S9(30).
-   01 INP PIC X(100).
-   01 OUT PIC X(100).
+    01 N   PIC S9(4).
+    01 M   PIC S9(4).
+    01 INP PIC X(4).
+    01 OUT PIC X(4).
 
 PROCEDURE DIVISION.
-   PERFORM
-      ACCEPT INP
-      UNSTRING INP
-          DELIMITED BY SPACE
-          INTO N, OUT
-      DISPLAY OUT
-      
+    PERFORM
+       ACCEPT INP
+       UNSTRING INP
+           DELIMITED BY SPACE
+           INTO N, OUT
+       DISPLAY function TRIM(OUT)
+       PERFORM N TIMES
+           ACCEPT INP
+       END-PERFORM
    END-PERFORM.
